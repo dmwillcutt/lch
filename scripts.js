4fbfa00569625ad9ebdcc717bc07b8e67e00362d
Dani W <willcut2@msu.edu>
2022-03-27 14:46:19 -0400

    Main commit

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var map = L.map('map').setView([42.73465, -84.54209], 16);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiZG13aWxsY3V0dCIsImEiOiJjazMwanlyNTEwOWc0M2tvNGMxcm90aThvIn0.SzFNAdPEsMo8yXKj7nDuvA}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v8',
+    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZG13aWxsY3V0dCIsImEiOiJjazMwanlyNTEwOWc0M2tvNGMxcm90aThvIn0.SzFNAdPEsMo8yXKj7nDuvA'
