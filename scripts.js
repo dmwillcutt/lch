89e327da1610bf33c3407fb917d397bfebc07ae3
Dani M. Willcutt <51728026+dmwillcutt@users.noreply.github.com>
2022-04-23 22:42:43 -0400

    adjusting max zoom

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([42.73342,-84.54792], 14);
+var map = L.map('map').setView([42.73342,-84.54792], 16);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG13aWxsY3V0dCIsImEiOiJjbDFoMGYzbmswMmJhM2xwYmQ3ZDFha2NxIn0.5lZY2hzSlHS69b2dXGVbrQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 20,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
