93f0252e0603d55ce2f1a5cabb2a4253a365715d
Dani M. Willcutt <51728026+dmwillcutt@users.noreply.github.com>
2022-05-02 22:04:29 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,10 +20,10 @@ L.marker([42.73403, -84.55119]).addTo(map)
     .bindPopup('<b>Bijou Restaurant</b> was a fine dining icon in downtown Lansing.')
     .openPopup();
 L.marker([42.73343, -84.54708]).addTo(map)
-    .bindPopup('<b>The Nuthouse Sports Grill</b>')
+    .bindPopup('<b>The Nuthouse Sports Grill</b> opened in 1996 alongside Lansing Lugnut Stadium - the city&#8217;s first AAA baseball stadium. “The Nuthouse,” as locals refer to it - stands out because of the large tower in the shape of a lugnut attached to the building. The stadium&#8217;s opening marked a wave of change for the downtown area. During the 1970s and 1908s much of East Michigan Avenue in front of the capitol building had been overtaken by the less savory elements of nightlife; namely adult bookstores, XXX movie theaters, and stripclubs. Revitalization during the 1990s welcomed Lansing-area families back downtown.')
     .openPopup();
 L.marker([42.73371, -84.54829]).addTo(map)
-    .bindPopup('<b>Dine&#8217;s Restaurant</b>')
+    .bindPopup('<b>Dine&#8217;s Restaurant</b> was opened and owned by husband and wife team George and Nevenka Dines. When they closed it, they bought the Cedar Village Shopping Center and ran a specialty food shop named Dines Gourmet and Gifts. Owned the Village Book Store too. The latter two ventures were with their daughter and two sons. Chef Jay Pilbeam. Near to the capitol building so it attracted a lot of government officials. The place included a cocktail lounge. Served cornish game hen, prime rib, broiled lobster tails. Could select a live lobster.')
     .openPopup();
 L.marker([42.73358, -84.54792]).addTo(map)
     .bindPopup('George and Charlie Sinadinos opened <b>The Knight Cap</b> in 1969. George died in 1988, but his wife, Charlie, kept the restaurant running until March 21, 2015. Charlie was helped by her sister, Windy Johnson, who served as the general manager of the Knight Cap for more than twenty-five years. The Knight Cap is one of many Lansing restaurants owned and operated by females.')
@@ -50,7 +50,7 @@ L.marker([42.73063, -84.55187]).addTo(map)
     .bindPopup('<b>Home Dairy Company</b>')
     .openPopup();
 L.marker([42.73733, -84.55359]).addTo(map)
-    .bindPopup('<b>Gregory&#8217;s Ice and Smoke</b>')
+    .bindPopup('<b>Gregory&#8217;s Ice and Smoke</b> was owned and operated by Gregory Eaton, who now owns Greogry&#8217;s Soul Food at the corner of Grand River Avenue and Martin Luther King, Jr. Boulevard. Eaton was the first African American business owner to receive a liquor license in Lansing. Liquor licenses are controlled by the state liquor commission.')
     .openPopup();
 L.marker([42.73008, -84.55203]).addTo(map)
     .bindPopup('<b>L&#8217;Aiglon Restaurant</b>')
